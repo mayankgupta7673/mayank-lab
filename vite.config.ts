9421cbca16f6420b8b532b0d389693de9e16a6da
Mayank Gupta <mayank.h.gupta@capgemini.com>
2026-09-13 16:23:10 +0530

    updating plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
+  plugins: [cloudflare()],
   build: {
     rollupOptions: {
       input: {
